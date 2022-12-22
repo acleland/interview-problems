c3d7288149b437a15010e1d3b6e1dea903d9254a
Andrea Cleland <andreaclelan@gmail.com>
2022-12-22 15:52:09 -0500

    added First Duplicate Value to array problems

---
+++ arrays/first_duplicate.py
@@ -0,0 +1,22 @@
+"""
+First Duplicate Value
+
+Given an array of integers between 1 an n inclusive, where n is the length
+of the array, write a function that returns the first integer that appears
+more than once (when the array is read from left to right).
+
+In other words, out of all the integers that might occur more than once in
+the input array, your function should return the one whose first duplicate value
+has the minimum index.
+
+If no integer appears more than once, your function should return -1.
+
+Note that you're allowed to mutate the input array.
+"""
+def firstDuplicateValue(array):
+    visited = set()
+    for n in array:
+        if n in visited:
+            return n
+        visited.add(n)
+    return -1
